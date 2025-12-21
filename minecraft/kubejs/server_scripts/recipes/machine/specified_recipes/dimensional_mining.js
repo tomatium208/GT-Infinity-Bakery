ffa86be1b971a71066f8944c8a8fc8c87a878962
tomato <shuthreekomusu@gmail.com>
2025-12-21 22:36:30 +0900

    更新！

---
+++ minecraft/kubejs/server_scripts/recipes/machine/specified_recipes/dimensional_mining.js
@@ -0,0 +1,10 @@
+ServerEvents.recipes(event => {
+
+    event.recipes.gtceu
+        .dimensional_mining("kjs/normal_run")
+        .circuit(1)
+        .itemOutputs("256x gtceu:valine3g_dust")
+        .duration(1000)
+        .EUt(VA.ZPM)
+
+})
