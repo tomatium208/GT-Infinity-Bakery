f31a5442f57871803faa75500f9288cbffe3da6f
P_nutsK <107553542+P-nutsK@users.noreply.github.com>
2025-12-22 18:48:24 +0900

    feat: nukelist

---
+++ minecraft/kubejs/client_scripts/nukelist.js
@@ -0,0 +1,17 @@
+/**
+ * Hides all objects on the nukeLists and items matching the Unification Patterns from JEI/EMI.
+ */
+JEIEvents.hideItems(event => {
+    global.itemNukeList.forEach(item => {
+        event.hide(item);
+    });
+    // Hide items matching the unification patterns.
+    // For definitions, see KubeJS/startup_scripts/unificationPatterns.js
+    // event.hide(global.unificationPattern)
+});
+
+JEIEvents.hideFluids(event => {
+    global.fluidNukeList.forEach(fluid => {
+        event.hide(fluid);
+    });
+});
---
+++ minecraft/kubejs/startup_scripts/nukelist/item.js
@@ -0,0 +1,9 @@
+/** @type {Internal.Ingredient_} */
+global.itemNukeList = [
+    /^ad_astra:.*desh.*$/,
+    /^ad_astra:.*etrium.*$/,
+    /^ad_astra:.*calorite.*$/,
+    /^ad_astra:.*ostrum.*$/,
+    /^ad_astra:.*calorite.*$/,
+    /^ad_astra:.*bucket.*$/,
+];
